b5a7e070db9962fbfe6fa36c13cd8340d32dc457
David L. Qiu <david@qiu.dev>
2025-08-25 14:58:00 +0200

    add jai-tool-call web component to show tool calls & outputs

--- packages/jupyter-ai/src/index.ts
+++ packages/jupyter-ai/src/index.ts
@@ -20,6 +20,7 @@ import { completionPlugin } from './completions';
 import { StopButton } from './components/message-footer/stop-button';
 import { statusItemPlugin } from './status';
 import { IJaiCompletionProvider } from './tokens';
+import { webComponentsPlugin } from './web-components';
 import { buildErrorWidget } from './widgets/chat-error';
 import { buildAiSettings } from './widgets/settings-widget';
 
@@ -124,6 +125,7 @@ export default [
   plugin,
   statusItemPlugin,
   completionPlugin,
+  webComponentsPlugin,
   stopStreaming,
   ...chatCommandPlugins
 ];
